23d0ac22dc28a01c9b837850c4c8a82cc137db99
534-PC\534 <nakatabjy@gmail.com>
2018-01-31 14:23:38 +0900

    출력 하기

--- src/carDBPrac/CarDBPracMain.java
+++ src/carDBPrac/CarDBPracMain.java
@@ -3,8 +3,8 @@ package carDBPrac;
 public class CarDBPracMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("출력");
 
-	}
+	} //end main
 
-}
+} //end CarDBPracMain class
