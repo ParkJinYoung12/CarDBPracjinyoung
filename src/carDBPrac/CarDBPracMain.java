d450505ee932cdaf55ea17f9070a72e92b1f78f6
534-PC\534 <nakatabjy@gmail.com>
2018-01-31 17:15:52 +0900

    서버에서 데이터 불러오기 구현 1번구현

--- src/carDBPrac/CarDBPracMain.java
+++ src/carDBPrac/CarDBPracMain.java
@@ -1,13 +1,14 @@
 package carDBPrac;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CarDBPracMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		//메뉴의 번호를 입력받아 지정한 번호 이외 다른번호를 입력하면 잘못 입력하셨습니다 출력후 다시 입력받게 함 
 		Scanner sc = new Scanner(System.in);
-		
+		CarInfoDAO dao = new CarInfoDAO();
 		while(true) {	
 		System.out.println("===========================");
 		System.out.println("번호를 입력하세요");
@@ -18,7 +19,13 @@ public class CarDBPracMain {
 		System.out.println("===========================");
 		int inputNum= sc.nextInt();
 		if(inputNum==1) { //자동차 정보 조회
+			ArrayList<CarInfo> list = dao.getCarInfoList();
 			
+			//조회한 데이터 뿌리기
+			for (int i = 0; i < list.size(); i++) {
+				CarInfo ci = list.get(i);
+				System.out.println(ci.getCiMaker());
+			}
 			break;
 		}
 		else if(inputNum==2) { //자동차 정보 입력
--- src/carDBPrac/CarInfoDAO.java
+++ src/carDBPrac/CarInfoDAO.java
@@ -1,5 +1,62 @@
 package carDBPrac;
 
-public class CarInfoDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
-}
+
+public class CarInfoDAO {
+	// 오라클 DBMS 접속에 필요한 정보들
+	
+	
+	private static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String ORACLE_USER_ID = "system";
+	private static final String ORACLE_USER_PW = "1234";
+	//접속 및 커리 처리에 필요한 객체들
+	private Connection conn =null;
+	private PreparedStatement pstmt =null;
+	private ResultSet rs = null;
+	
+	//모든자동차 정보를 조회한다.
+	public ArrayList<CarInfo> getCarInfoList() throws Exception{
+		ArrayList<CarInfo> list = new ArrayList<>();
+		
+		try {
+			//dbms 접속정보 읽어오기
+			conn=DriverManager.getConnection(ORACLE_URL,ORACLE_USER_ID,ORACLE_USER_PW);
+			//1.sql문을 String으로 준비
+			String sql = " select * from carInfo"; //리터럴에 ; 이게(세미콜론) 들어가면 안된다 
+			//2.쿼리 실행 준비
+			pstmt = conn.prepareStatement(sql);
+			//3.실행 결과를 ResultSet 객체에 담는다.
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				CarInfo cInfo=new CarInfo();
+				cInfo.setCiNum(rs.getInt(1));
+				cInfo.setCiName(rs.getString(2));
+				cInfo.setCiMaker(rs.getString(3));
+				cInfo.setCiPrice(rs.getInt(4));				
+				list.add(cInfo);
+				
+			}
+			
+		} catch (Exception e) {
+			
+		}finally {
+			conn.close();
+			pstmt.close();
+			rs.close();
+		}
+		
+		return list;
+	}
+	
+	
+	
+	
+}//class end ~!
